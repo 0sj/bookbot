e5021f510c141099f363f41aa44500f418bcd347
0sj <jaydnisswag@gmail.com>
2025-01-17 15:31:14 +1300

    made the code work

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as frankenstein:
+        file_contents = frankenstein.read()
+        
+        file_contents = split_text(file_contents)
+        print(len(file_contents))   
+        return file_contents
+
+def split_text(text):
+    return text.split()
+
+
+main()
