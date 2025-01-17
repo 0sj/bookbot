06f42c534a4825d1f8bb546cdd9eb0865a0f6b17
0sj <jaydnisswag@gmail.com>
2025-01-17 21:26:13 +1300

    meow

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 def main():
-    with open("books/frankenstein.txt") as frankenstein:
-        file_contents = frankenstein.read()
-        
-        file_contents = split_text(file_contents)
-        print(len(file_contents))   
-        return file_contents
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
-def split_text(text):
-    return text.split()
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 
 main()
